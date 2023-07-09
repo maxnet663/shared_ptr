f6dad2f2fac264c2eada66958b1d25a08bfd6232
Maxim Akimenko <maxnet663@gmail.com>
2023-07-09 14:53:49 +0300

    optimized

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -12,25 +12,20 @@ class ControlBlock {
     friend SharedPtr<T>;
     size_t refs_counter;
     T *ptr;
-    void add_ref() { refs_counter++; }
+
     ControlBlock(const ControlBlock &other); //banned
-    //default constructor
-    ControlBlock() : ptr(0), refs_counter(0) {}
+
     //constructor for make_shared
     ControlBlock(T *in_ptr) : ptr(std::move(in_ptr)), refs_counter(1) {}
-    //for following constructor T must have copy constructor
-    ControlBlock(T &obj) : ptr(new T(std::move(obj))), refs_counter(1) {}
+
     ~ControlBlock() {}
+
     size_t getRefsCount() { return refs_counter; }
     void release();
+    void add_ref() { refs_counter++; }
 
-    //he is really friendly
-    template <class U>
-    friend SharedPtr<U> make_shared(const U& obj);
-    template <class U>
-    friend SharedPtr<U> make_shared(const SharedPtr<U>& other);
-    template <class U>
-    friend SharedPtr<U> make_shared(U *ptr);
+    template <class U, class... Args>
+    friend SharedPtr<U> make_shared(Args&&... args);
 };
 
 template <class T>
@@ -45,9 +40,6 @@ public:
     SharedPtr() : cb_ptr(0) {}
 
     SharedPtr(const SharedPtr<T> &other);
-
-    // for following constructor T must have copy constructor
-    SharedPtr(T &obj) : cb_ptr(new ControlBlock<T>(obj)) {} // std::move in new?
 
     ~SharedPtr() { if (cb_ptr) cb_ptr->release(); }
 
@@ -64,39 +56,16 @@ public:
     const T* operator->() const { return cb_ptr->ptr; }
     const T& operator*() const { return *(cb_ptr->ptr); }
 
-    template <class U>
-    friend SharedPtr<U> make_shared(const U& obj);
-    template <class U>
-    friend SharedPtr<U> make_shared(const SharedPtr<U>& other);
-    template <class U>
-    friend SharedPtr<U> make_shared(U *ptr);
+    template <class U, class... Args>
+    friend SharedPtr<U> make_shared(Args&&... args);
 };
 
-// T must have copy constructor
-template <class T>
-SharedPtr<T> make_shared(const T& obj) {
-    auto p = new ControlBlock<T>(new T(std::move(obj)));// std::move in new?
-    return SharedPtr<T>(p);
+template <class T, class... Args>
+SharedPtr<T> make_shared(Args&&... args) {
+    auto p_t = new T(std::forward<Args>(args)...);
+    auto p_cb = new ControlBlock<T>(p_t);
+    return SharedPtr<T>(p_cb);
 }
-
-template <class T>
-SharedPtr<T> make_shared(const SharedPtr<T>& other) {
-    return SharedPtr<T>(other);
-}
-
-//following func assumes the pointer came from heap
-// if it is not, it will be sad
-template <class T>
-SharedPtr<T> make_shared(T *ptr) {
-    auto p = new ControlBlock<T>(ptr);
-    return SharedPtr<T>(p);
-}
-
-//will be soon
-//template <class T, class... Args>
-//SharedPtr<T> make_share(Args&&... args) {
-//    auto p = ne
-//}
 
 
 #endif // SHARED_PTR_H_SENTRY
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 
 int main() {
     int x = 25;
-    SharedPtr<int> a(x);
+    SharedPtr<int> a = make_shared<int>(x);
     SharedPtr<int> b(a);
     SharedPtr<int> f = make_shared<int>(1);
-    SharedPtr<item> c = make_shared<item>(item(x, 2));
-    SharedPtr<item> d = make_shared<item>(c);
+    SharedPtr<item> c = make_shared<item>(x, *f);
+    SharedPtr<item> d = c;
     c.release();
-    c = make_shared<item>(new item(33, 44));
+    c = make_shared<item>(33, 44);
 }
